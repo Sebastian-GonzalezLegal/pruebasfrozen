aea8d18562a2813411f4881e6fe8ae5362ee839a
Sebastian Gonzalez Legal <109675467+Sebastian-GonzalezLegal@users.noreply.github.com>
2025-09-27 15:54:46 -0300

    Delete app directory

--- app/schemas/responses.py
+++
@@ -1,9 +0,0 @@
-from marshmallow import Schema, fields
-
-class ResponseSchema(Schema):
-    """Esquema para respuestas estandarizadas"""
-    success = fields.Bool(required=True)
-    data = fields.Raw(allow_none=True)
-    message = fields.Str(allow_none=True)
-    error = fields.Str(allow_none=True)
-    details = fields.Dict(allow_none=True)
